f11977b452569467848131f974c6c47a024e8133
haku-ohsumi <haku818181@gmail.com>
2023-08-19 16:17:03 +0900

    nodeとmysqlを繋げたよ

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = 3001;
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'Haku_0717',
+  database: 'memo_app'
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/api', (req, res) => {
-  res.json({message: "Hello World"});
-})
+  connection.query(
+    'SELECT * FROM users',
+    function(err, results, fields){
+      if(err){
+        console.log('接続エラー');
+        throw err;
+      }
+      res.json({message: results[0].name});
+    }
+  )
+});
 
 app.listen(port, () => {
   console.log(`listening on *:${port}`);
