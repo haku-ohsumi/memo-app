e5fa560cb54fa2ec06b2d6202db4f60213c958e4
haku-ohsumi <haku818181@gmail.com>
2023-08-19 18:54:25 +0900

    めっちゃcss書いたよ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import Header from './modules/Header.jsx';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  },[])
-
-  return (
-    <div className = "App">
-      <h1>フロントエンド</h1>
-      <p>{ message }</p>
-    </div>
+ return (
+    <React.StrictMode>
+      <Header/>
+    </React.StrictMode>
   );
 }
 
